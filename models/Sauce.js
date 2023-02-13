001f28e6cc37748eb9e54c2ddace7b210d43bb28
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-02-13 22:03:09 +0100

    sauces model

---
+++ models/Sauce.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  like: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
