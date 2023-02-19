60306fbea5c73b7929df7295eadd209fba53128c
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-02-19 17:09:45 +0100

    sauces model modified

--- models/Sauce.js
+++ models/Sauce.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
   name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
+  mainPepperIngredient: { type: String, required: true },
+  heat: { type: Number, required: true},
   userId: { type: String, required: true },
   like: { type: Number, required: true },
 });
