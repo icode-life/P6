c2c42722bbbefb1346b789439b79cfb7e283da49
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-02-19 21:15:06 +0100

    ajout likes/dislikes au model sauce

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String, required: false },
   mainPepper: { type: String, required: true },
   heat: { type: Number, required: true},
-  userId: { type: String, required: true },
-  like: { type: Number, required: false },
+  likes: { type: Number, required: false },
+  dislikes: {type: Number, required: false},
+  likesUserIds: {type: String, required: false},
+  dislikesUserIds: {type: String, required: false}
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
